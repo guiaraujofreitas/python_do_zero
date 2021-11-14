101f55066fd75830accb7e6b38a6301b1ad6c136
guiaraujofreitas <guiaraujofreitas@outlook.com>
2021-11-14 18:05:15 -0300

    Terminado os exercicios de estrutura

--- defs_aula.py
+++ defs_aula.py
@@ -1,25 +1,22 @@
-import time #library to control the time
+import time
 from geopy.geocoders import Nominatim
 
-geolocator = Nominatim(user_agent= 'geopiExercise')
+geolocator = Nominatim( user_agent='geopiExercises' )
 
-def get_longlat (x):
-    
-    index,row = x
-    time.sleep (1)
-    
-    #chamada api
-    response = geolocator.reverse(row['query'])
-    address = geolocator.reverse.raw['address']
-    
+def get_longlat( x ):
+    index, row = x
+    time.sleep(1)
+    response = geolocator.reverse( row['query'] )
+    address = response.raw['address']
+
     try:
-        place_id= response.raw['place_id'] if 'place_id' in response.raw else 'NA'
-        osm_type= response.raw['osm_type'] if 'osm_type' in response.raw else 'NA'
-        country= response.raw['address']['country'] if 'country' in response.raw ['address']else 'NA'
-        country_code= response.raw['address']['country_code'] if 'country_code' in response.raw['address'] else 'NA'
-        
-        return place_id,osm_type,country,country_code
-    
-    except:
-           return None, None, None, None
+        place_id = response.raw['place_id'] if 'place_id' in response.raw else 'NA'
+        osm_type = response.raw['osm_type'] if 'osm_type' in response.raw else 'NA'
 
+        country = address['country'] if 'country' in address else 'NA'
+        country_code = address['country_code'] if 'country_code' in address else 'NA'
+
+        return place_id, osm_type, country, country_code
+
+    except:
+        return None, None, None, None
