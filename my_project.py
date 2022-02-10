21a74e2d49d02c6f2e26291e5c564ca3e83d41e4
guiaraujofreitas <guiaraujofreitas@outlook.com>
2022-02-09 21:15:57 -0300

    making filters

--- my_project.py
+++ my_project.py
@@ -49,89 +49,13 @@ st.title(" Overview")
 
 st.dataframe(df)
 
-# 1ª Hipotese
-f_h1 = df[['waterfront','price_m2_lot']].groupby('waterfront').median().reset_index()
-
-#filtrando as casas c/ vista p/ o mar
-a = f_h1.loc[f_h1['waterfront']== 0]
-
-#filtrando as casas s/ vista para o mar
-b = f_h1.loc[f_h1['waterfront']!= 0]
-
-#calculando a diferença em % / pegando o primeiro valor de cada característica do imóvel
-
-porcentagem = diferenca(vf=b['price_m2_lot'].iloc[0], vi=a['price_m2_lot'].iloc[0])
-
-print('A diferença é de {} %'.format(porcentagem))
-
-#plotando o gráfico
-
-#dimensão do gráfico
-#fig = plt.figure(figsize=(13,8))
-
-#sns.barplot(x= 'waterfront', y='price_m2_lot', data = f_h1);
-
-figure= px.bar(f_h1, x= 'waterfront', y='price_m2_lot' )
-st.plotly_chart(figure)
-
+#
 ############################################################################################
 
-#============= Filtros ===================== #
-
-min_year_built = int(df['yr_built'].min())
-max_year_built = int(df['yr_built'].max())
 
 
-st.sidebar.title ('Commercial Options') #título do filtro
-st.title (' Commercial Attributes')
 
-st.sidebar.subheader('Select Max Year Built') #texto em cima do filtro
-
-f_year_built = st.sidebar.slider('Year Built', min_year_built,
-                                            max_year_built,
-                                            min_year_built )
-
-
-#agrupando e criando os filtros das casas construidas.
-f_h2 = df[['yr_built','price_m2_lot']].groupby('yr_built').median().reset_index()
-
-# filtrando os imóveis antes do ano de 1955
-menor = f_h2.loc[f_h2['yr_built'] < '1955']
-
-#filtrando os imóveis construindo desde de 1955
-maior = f_h2.loc[f_h2['yr_built'] >= '1955']
-
-#descobriando a mediana de preços dos imóveis mais antigo
-antigo = menor.median(numeric_only=True).iloc[0]
-
-#descobrindo a mediana de preços dos imóveis mais novos
-novo   = maior.median(numeric_only=True).iloc[0]
-
-## making the plot 
-
-c1, c2, c3 = st.columns(3)
-
-c1.header('Prices M2 Houses by Year of Built')
-
-fig = px.bar(f_h2,x='yr_built', y='price_m2_lot')
-c1.plotly_chart(fig, user_container_witdh= True)
-
-fig_teste = px.scatter(f_h2, x='yr_built',y= 'price_m2_lot')
-c1.plotly_chart(fig_teste,user_container_witdh= True)
-
-c2.header('Prices M2 of Smalll of Houses Built')
-
-fig1 = px.bar(maior, x='yr_built', y='price_m2_lot')
-c2.plotly_chart(fig1,user_container_witdh= True)
-
-fig_teste = px.scatter(menor, x='yr_built',y= 'price_m2_lot')
-c2.plotly_chart(fig_teste,user_container_witdh= True)
-
-c3.header('Prices M2 of Beyourd of Houses Built')
-fig2 = px.bar(menor, x='yr_built', y='price_m2_lot')
-c3.plotly_chart(fig2, user_container_witdh= True)
-fig_teste = px.scatter(maior, x='yr_built',y= 'price_m2_lot')
-c3.plotly_chart(fig_teste, user_container_witdh= True)
+# ===================================================================== #
 
 
 ##### TABLE OF ACQUISTION ######
@@ -164,7 +88,7 @@ df1 = pd.merge(df1,seasons_median, on= ['zipcode','numeric_seasons'], how='inner
 #TABLE ACQUISITION OF HOUSES
 
 #loading date
-location = df1.head(50).copy(deep=True)
+location = df1.head(320).copy(deep=True)
 
 #select columns necessary
 location = location[['id','zipcode','price','price_region','price_seasons','condition',
@@ -202,35 +126,26 @@ for i in range(len(location)):
                                                  & (x['waterfront']==1) & (x['condition']>=3) else 'no_buy', axis=1)
        
     tabela = location[['id','zipcode','price','price_region','condition','waterfront','state',
-                       'city','neighbourhood','acquisition']].copy(deep=True)
+                       'city','neighbourhood','acquisition','lat','long']].copy(deep=True)
 
 st.title( 'TABLE OF ACQUISITION OF HOUSES')
 st.dataframe(tabela)
 
-######## TABLE SELL OF HOUSES 
 
-st.title( 'TABLE SELL OF HOUSES')
+### Filtro map #######
 
-#linha que calculo o preço de venda dos imóveis:
-tabela['sell']= tabela.apply(lambda x: (x['price']* 0.30 + x['price']) if x['acquisition']=='buy' 
-                                 else (x['price'] *0.10 + x['price']),axis=1)
 
-#linha que calculo a lucratividade dos imóveis
-tabela['lucro'] = tabela.apply(lambda x: x['sell'] - x['price'],axis=1)
+##========= MAP ================
+houses = tabela[['id','price','lat','long']].copy(deep=True)
 
-#criando coluna com as respectivas porcentagens de lucro das vendas dos imóveis
-tabela['porcentagem_lucro'] = diferenca(vf=tabela['sell'],vi=tabela['price'])
-
-table_sell = tabela.copy(deep=True)
-
-st.dataframe(table_sell)
-
-#========= MAP ================
-houses = df1[['id','price','lat','long']].copy()
-
-houses = px.density_mapbox(df, lat='lat', lon='long',z= 'price_m2_lot',radius=10,zoom=8,
+houses = px.density_mapbox(tabela, lat='lat', lon='long',z= 'price',radius=10,zoom=8,
                             mapbox_style="stamen-terrain")
 
 houses.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 st.plotly_chart(houses,user_container_witdh= True)
+
+
+######## TABLE SELL OF HOUSES #################
+
+
 
